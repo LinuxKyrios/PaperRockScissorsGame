e95a1123b8d62bf2d426c3cce6f3a26443a98b8f
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-18 21:44:51 +0100

    Created first array and function with random number choose

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val options = arrayOf("Rock", "Paper", "Scissors")
+    // Creating val array, to remain unchanged
+    val gameChoice = getGameChoice(options)
+    //calling function getGameChoice
 }
+
+fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+//Function created for random choose value at start.
