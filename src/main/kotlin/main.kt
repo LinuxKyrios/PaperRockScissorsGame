c0754f7cde0b99b59cf347de5694c99893b1c47b
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-18 23:59:19 +0100

    Completed code of the Paper Rock Scissors game

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,40 @@ fun main(args: Array<String>) {
     // Creating val array, to remain unchanged
     val gameChoice = getGameChoice(options)
     //calling function getGameChoice
+    val userChoice = getUserChoice(options)
+    //Calling getUserChoice function\
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
 //Function created for random choose value at start.
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+    //user input uploading loop - finished when provided input is correct
+    while (!isValidChoice) {
+        //asking user for choice
+        print("Please choose one of the option:")
+        for(item in optionsParam) print(" $item")
+        println(".")
+        //Uploading user input
+        val userInput = readLine()
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+        //if chosen option is not valid
+        if (!isValidChoice) println("Please choose on of available option")
+    }
+    return userChoice
+}
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+    //Checking result
+    if (userChoice == gameChoice) result = "Remis!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock") ||
+            (userChoice == "Scissors" && gameChoice == "paper")) result = "You won!"
+    else result = "You lost :c"
+    println("You have choose $userChoice, and I have choose $gameChoice. $result")
+}
